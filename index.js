a46201ad5d74ed5a36997462d657ff1e07979a5d
Matheria <sensenevercame@gmail.com>
2022-11-09 20:59:27 +0500

    feat: add theme change func

--- index.js
+++ index.js
@@ -1,48 +1,21 @@
-const input = document.querySelector(".js-input");
-const form = document.querySelector("form");
-const loadBtn = document.querySelector(".js-load");
-const resultsContainer = document.querySelector(".results-container");
+const body = document.body;
+const btnLight = document.querySelector(".btn_light");
+const btnDark = document.querySelector(".btn_dark");
 
-// Валидация input
-const inputValidation = () => {
-  let inputValue = input.value.trim();
-
-  try {
-    if (inputValue === "") throw new Error("Заполните поле ввода");
-    if (!Number(inputValue)) throw new Error("Введите числовое значение");
-    if (inputValue.length > 10 || inputValue.length < 5)
-      throw new Error(`Число должно быть в интервале от 5 до 10`);
-
-    resultsContainer.innerHTML = inputValue;
-  } catch (err) {
-    resultsContainer.innerHTML = `<p>${err.message}</p>`;
-  }
+const handleThemeChange = () => {
+  localStorage.getItem("dark") === "true"
+    ? body.classList.add("body_dark")
+    : body.classList.remove("body_dark");
 };
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  inputValidation();
+btnDark.addEventListener("click", () => {
+  localStorage.setItem("dark", "true");
+  handleThemeChange();
 });
 
-// Функция lottery на async/await
-const lottery = async () => {
-  console.log("Вы начали игру");
+btnLight.addEventListener("click", () => {
+  localStorage.setItem("dark", "false");
+  handleThemeChange();
+});
 
-  let promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      Math.random() > 0.5 ? resolve() : reject();
-    }, 1000);
-  });
-
-  try {
-    await promise;
-    console.log("Вы выиграли");
-    console.log("Вам заплатили");
-  } catch {
-    console.log("Вы проиграли");
-  } finally {
-    console.log("Игра закончена");
-  }
-};
-
-lottery();
+handleThemeChange();
